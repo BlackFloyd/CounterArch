67437222d1082f70a96b22ffd3070864b180a61d
RawBin <root.serve@googlemail.com>
2018-12-14 14:29:55 +0100

    Exit on all errors

--- build.sh
+++ build.sh
@@ -43,27 +43,35 @@ then
     exit 1
 fi
 
+set -e
 echo Reseting...
-git reset --hard $LOCAL &&
-git pull &&
-echo Hacking Makefiles to enable the whole instruction set... &&
-sed -i -e 's/-march=core2/-march=native/g' arch/x86/Makefile &&
-sed -i -e 's/-march=atom/-march=native/g' arch/x86/Makefile &&
-echo Copying resources to base... &&
-make clean &&
-zcat /proc/config.gz > .config &&
-make nconfig &&
-make CC="ccache gcc" -j$(nproc --all) &&
-read -p "Press enter to install the kernel modules" &&
-sudo make modules_install &&
+git reset --hard $LOCAL
+git pull
+echo Hacking Makefiles to enable the whole instruction set...
+sed -i -e 's/-march=core2/-march=native/g' arch/x86/Makefile
+sed -i -e 's/-march=atom/-march=native/g' arch/x86/Makefile
+echo Copying resources to base...
+make clean
+zcat /proc/config.gz > .config
+make nconfig
+make CC="ccache gcc" -j$(nproc --all)
+read -p "Press enter to install the kernel modules"
+sudo make modules_install
+
 if [ -f /boot/vmlinuz-linux-counterarch ]; then
-	read -p "Press enter to back up the old kernel" &&
-	sudo cp -v /boot/vmlinuz-linux-counterarch /boot/vmlinuz-linux-counterarch-previous
-	sudo cp -v /boot/initramfs-linux-counterarch.img /boot/initramfs-linux-counterarch-previous.img
+    read -p "Press enter to back up the old kernel"
+    sudo cp -v /boot/vmlinuz-linux-counterarch /boot/vmlinuz-linux-counterarch-previous
+    sudo cp -v /boot/initramfs-linux-counterarch.img /boot/initramfs-linux-counterarch-previous.img
 fi
-read -p "Press enter to install new kernel" &&
-sudo cp -v arch/x86_64/boot/bzImage /boot/vmlinuz-linux-counterarch &&
-sudo mkinitcpio -p linux-counterarch &&
-read -p "Press enter to reinstall kernel modules" &&
-sudo dkms autoinstall -k $(make kernelrelease) &&
+
+read -p "Press enter to install new kernel"
+sudo cp -v arch/x86_64/boot/bzImage /boot/vmlinuz-linux-counterarch
+
+if [ ! -f /etc/mkinitcpio.d/linux-counterarch.preset ]; then
+    sudo cp -v ../linux-counterarch.preset /etc/mkinitcpio.d/	
+fi
+
+sudo mkinitcpio -p linux-counterarch
+read -p "Press enter to reinstall kernel modules"
+sudo dkms autoinstall -k $(make kernelrelease)
 echo Done.
