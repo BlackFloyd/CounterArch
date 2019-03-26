34576b91abba96b1581c0f355eaabf49234711d4
RawBin <root.serve@googlemail.com>
2019-03-26 05:34:58 +0100

    Actually tested the grub-mkconfig

--- build.sh
+++ build.sh
@@ -125,19 +125,23 @@ fi
 
 if $FIRST -eq true; then
     read -p "Perform grub-mkconfig? (y/N)" -n 1 -r
+    echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         if [ ! -f /boot/grub/grub.cfg ]; then
             echo -e "\e[31mError: \e[39mI could not find your previous grub config under /boot/grub/grub.cfg. As this is the only compatible grub.cfg path I won't do anything. Please configure grub manually."
         else
             echo "Running grub-mkconfig..."
-            grub-mkconfig -o /tmp/counterarch-grub.cfg
+            sudo grub-mkconfig -o /tmp/counterarch-grub.cfg
+            sudo chown $(whoami) /tmp/counterarch-grub.cfg
             read -p "Please press return and have a look at your new grub.cfg."
+            echo
             ${EDITOR:-vi} /tmp/counterarch-grub.cfg
             read -p "Apply new config? (y/N)" -n 1 -r
+            echo
             if [[ $REPLY =~ ^[yY]$ ]]; then
                 echo "Backing up old config to /boot/grub/grub.cfg.old"
                 sudo cp /boot/grub/grub.cfg /boot/grub/grub.cfg.old
-                echo "Applying new config..."
+                echo "Applying new config... Ignore errors about preserving the ownership - this is actually intended..."
                 sudo mv /tmp/counterarch-grub.cfg /boot/grub/grub.cfg
             fi
         fi
